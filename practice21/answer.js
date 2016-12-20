cb0b308bac65bb43fb54d3cbd182facbfe4bec6a
sgawne2 <sgawne2@gmail.com>
2016-12-19 17:49:19 -0800

    practice 1.6 - Bipolar Loop

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,23 @@
-answer.js
+/*
+ Bipolar Loop
+
+ Create a single loop that simultaneously counts up on one variable, while counting down on another.  The only code inside the for loop should be a console.log that lists both variables.
+
+ #### Put your solution in answer.js
+
+ - hint: You can have multiple initiators, conditionals, and incrementers/decrementers in the same <em>for</em> loop.  For example:
+
+ ```
+ for(<initiator 1>,<initiator 2>; <check 1>,<check2>; <increment 1>,<increment 2>){
+ //do work
+ }
+ ```
+
+ */
+
+function bipolar_loop() {
+    for (var i = 0, j = 10; i <= 10, j >= 0; i++, j--) {
+        console.log(i, j);
+    }
+}
+bipolar_loop();
